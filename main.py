07d709ac99cfcc880d3c49c377b301c69420aedc
Alexey Kibyakov <lexa111lexa6@mail.ru>
2022-11-30 09:29:07 +0300

    change salary

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_params():
 def get_links(url):
     res_list = []
     attr_comp = {'data-qa': "vacancy-serp__vacancy-employer"}
-    
+    regexp_salary = re.compile(r'(от\s)|(до\s)*\s?(\d+)\\xa?0{1}(0{3})(\s*[A-Z]*[а-я]*\D*)')
     req = requests.get(url=url, headers=get_headers(), params=get_params())
     soup = BeautifulSoup(req.text, 'lxml')
     items_links = soup.find_all("a", class_="serp-item__title")
@@ -26,8 +26,8 @@ def get_links(url):
         res_list.append({
             'title': item.contents[0],
             'link': item.attrs['href'],
-            'company': items_companies[i].contents[0],
-            'salary': get_salary(item.attrs['href'])
+            'company': items_companies[i].text,
+            'salary': regexp_salary.sub(r'\1\2\3\4\5', get_salary(item.attrs['href']))
             })
     return res_list
 
@@ -35,13 +35,9 @@ def get_salary(url):
     req = requests.get(url=url, headers=get_headers())
     soup = BeautifulSoup(req.text, 'lxml')
     salary = soup.find("span", class_="bloko-header-section-2 bloko-header-section-2_lite")
-    if salary:
-        regexp_sub = re.sub(r'(\d+)\\xa?0{1}(0{3})', r'\1\2', salary.text)
-        return regexp_sub
-    elif salary.text == 'з/п не указана':
-        return salary.text
-    else:
-        return 'з/п не указана'
+    text = salary.text
+    
+    return text
 
 if __name__ == "__main__":
     url = 'https://spb.hh.ru/search/vacancy'
